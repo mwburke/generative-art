36db50d7aac334e7dd7aebd31838bdde54dd27ef
Matthew Burke <mburke@creditsesame.com>
2018-07-25 19:23:23 -0700

    014 and 015

---
+++ js/posts-js/014.js
@@ -0,0 +1,59 @@
+var degree_jump = 10;
+var min_width = 20;
+var max_width = 250;
+
+var big_degree_jump = 30;
+var big_min_width = 320;
+var big_max_width = 350;
+
+var j = 0;
+
+function setup() {
+    var myCanvas = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+    myCanvas.parent('viz014');
+    noLoop();
+}
+
+function draw() {
+    translate(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2);
+    generate();
+    j++;
+}
+
+
+function generate() {
+    strokeWeight(2);
+    for (var i = 0; i < 180; i += degree_jump) {
+
+        rotate(radians(degree_jump));
+        fill(0, 0, 0, 0);
+        stroke(generate_color(i));
+        ellipse(0, 0, min_width, max_width);
+    }
+
+    for (var i = 0; i < 180; i += big_degree_jump) {
+        strokeWeight(1);
+        rotate(radians(big_degree_jump));
+        fill(0, 0, 0, 0);
+        stroke(generate_color(i));
+        ellipse(0, 0, big_min_width, big_max_width);
+    }
+}
+
+
+function generate_color(degree) {
+    ellipse_color = color(d3.interpolateRainbow(degree / 180)).levels;
+    ellipse_color[3] = 150;
+    return color(ellipse_color);
+}
+
+
+function mousePressed() {
+    if ((j % 2) == 0) {
+        background(247, 247, 247);
+    } else {
+        background(50, 50, 50);
+    }
+    generate();
+    j++;
+}
---
+++ js/posts-js/015.js
@@ -0,0 +1,49 @@
+var radius = 100;
+var jump_length = 150;
+var poly_sides = 6;
+var triangles = [];
+
+
+function setup() {
+    var myCanvas = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+    myCanvas.parent('viz015');
+
+    triangles.push(new jump_triangle(color(255, 0, 0, 100), 1, 1));
+    triangles.push(new jump_triangle(color(0, 255, 0, 100), -1, 1));
+    triangles.push(new jump_triangle(color(0, 0, 255, 100), 0, -1));
+}
+
+function draw() {
+    blendMode(BLEND);
+    background(247, 247, 247);
+    strokeWeight(5);
+    blendMode(MULTIPLY);
+    for (i = 0; i < triangles.length; i++) {
+        triangles[i].move();
+        triangles[i].display()
+    }
+}
+
+
+function jump_triangle(triangle_color, jump_lr, jump_ud) {
+    this.triangle_color = triangle_color;
+    this.jump_lr = jump_lr;
+    this.jump_up = jump_ud;
+    this.x = CANVAS_WIDTH / 2;
+    this.y = CANVAS_HEIGHT / 2;
+    this.rot = 90;
+
+    this.move = function() {
+        if (((frameCount % 120) > 60) | (frameCount % 120 == 0)) {
+            this.x = CANVAS_WIDTH / 2 + sin(radians((frameCount % 60) / 60 * 180)) * jump_length * jump_lr;
+            this.y = CANVAS_WIDTH / 2 + sin(radians((frameCount % 60) / 60 * 180)) * jump_length * jump_ud;
+            this.rot += 1;
+        }
+    }
+
+    this.display = function() {
+        stroke(triangle_color);
+        fill(0, 0, 0, 0);
+        polygon(this.x, this.y, radius, poly_sides, this.rot);
+    }
+}
